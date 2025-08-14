a8b42ada9e08c7a0a0da1e3b6c1eccef6d1db100
divyanshucse17 <divyanshu.cse17@gmail.com>
2025-08-14 09:16:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,49 +1,33 @@
 function convertToRoman(num) {
-  const obj = {
-    0: ['M', 1000], 
-    1: ['D', 500], 
-    2: ['C', 100], 
-    3: ['L', 50], 
-    4: ['X', 10], 
-    5: ['V', 5], 
-    6: ['I', 1]
-  };
-
-  // Special cases for subtractive notation
-  const special = [
-    ['CM', 900],
-    ['CD', 400],
-    ['XC', 90],
-    ['XL', 40],
-    ['IX', 9],
-    ['IV', 4]
-  ];
-
-  let result = "";
-
-  // Handle special cases first
-  for (let [sym, val] of special) {
-    while (num >= val) {
-      result += sym;
-      num -= val;
+    const romanSymbols = [
+        ['M', 1000],
+        ['CM', 900],
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
+        ['I', 1]
+    ];
+    
+    let result = '';
+    
+    for (let i = 0; i < romanSymbols.length; i++) {
+        const [symbol, value] = romanSymbols[i];
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
     }
-  }
-
-  // Handle normal numerals from obj
-  for (let key in obj) {
-    let [sym, val] = obj[key];
-    while (num >= val) {
-      result += sym;
-      num -= val;
-    }
-  }
-
-  return result;
+    
+    return result;
 }
 
-// Example test cases
-// console.log(convertToRoman(14));  // XIV
-// console.log(convertToRoman(798)); // DCCXCVIII
-
-// do not edit below this line
-module.exports = convertToRoman;
+// Example usage:
+// console.log(convertToRoman(36)); // Output: XXXVI
+// console.log(convertToRoman(798)); // Output: DCCXCVIII
