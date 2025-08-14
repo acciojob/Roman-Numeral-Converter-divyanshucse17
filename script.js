c3fe2eb28ab1648c97060d3ecfdd6f8fccd02a37
divyanshucse17 <divyanshu.cse17@gmail.com>
2025-08-14 09:14:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  const obj = {
+    0: ['M', 1000], 
+    1: ['D', 500], 
+    2: ['C', 100], 
+    3: ['L', 50], 
+    4: ['X', 10], 
+    5: ['V', 5], 
+    6: ['I', 1]
+  };
 
-  //your code here
+  // Special cases for subtractive notation
+  const special = [
+    ['CM', 900],
+    ['CD', 400],
+    ['XC', 90],
+    ['XL', 40],
+    ['IX', 9],
+    ['IV', 4]
+  ];
 
+  let result = "";
+
+  // Handle special cases first
+  for (let [sym, val] of special) {
+    while (num >= val) {
+      result += sym;
+      num -= val;
+    }
+  }
+
+  // Handle normal numerals from obj
+  for (let key in obj) {
+    let [sym, val] = obj[key];
+    while (num >= val) {
+      result += sym;
+      num -= val;
+    }
+  }
+
+  return result;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
-
-
-
+// Example test cases
+// console.log(convertToRoman(14));  // XIV
+// console.log(convertToRoman(798)); // DCCXCVIII
 
 // do not edit below this line
-module.exports = convertToRoman
+module.exports = convertToRoman;
