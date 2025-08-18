bf3f93b9374f060331b11ce11616eb0c79a10ed4
divyanshucse17 <divyanshu.cse17@gmail.com>
2025-08-18 08:44:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,54 @@
-function convertToRoman(num) {
-    if (num === 0) return ""; // Romans didn't have a numeral for zero
-    
-    const romanSymbols = [
-        ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"], // 1-9
-        ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"], // 10-90
-        ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"], // 100-900
-        ["", "M", "MM", "MMM", "Mↁ", "ↁ", "ↁM", "ↁMM", "ↁMMM", "Mↂ"], // 1000-9000
-        ["", "ↂ", "ↂↂ", "ↂↂↂ", "ↂↇ", "ↇ", "ↇↂ", "ↇↂↂ", "ↇↂↂↂ", "ↂↈ"], // 10000-90000
-        ["", "ↈ"] // 100000
-    ];
-    
-    // Special cases for larger numbers (using Unicode Roman numeral symbols)
-    if (num >= 100000) {
-        return "ↈ".repeat(Math.floor(num / 100000)) + convertToRoman(num % 100000);
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function toRoman(num) {
+  if (num === 0) return "N"; // Romans sometimes used N for zero
+
+  const romans = [
+    { value: 100000, symbol: "C̅" },
+    { value: 90000, symbol: "X̅C̅" },
+    { value: 50000, symbol: "L̅" },
+    { value: 40000, symbol: "X̅L̅" },
+    { value: 10000, symbol: "X̅" },
+    { value: 9000, symbol: "I̅X̅" },
+    { value: 5000, symbol: "V̅" },
+    { value: 4000, symbol: "I̅V̅" },
+    { value: 1000, symbol: "M" },
+    { value: 900, symbol: "CM" },
+    { value: 500, symbol: "D" },
+    { value: 400, symbol: "CD" },
+    { value: 100, symbol: "C" },
+    { value: 90, symbol: "XC" },
+    { value: 50, symbol: "L" },
+    { value: 40, symbol: "XL" },
+    { value: 10, symbol: "X" },
+    { value: 9, symbol: "IX" },
+    { value: 5, symbol: "V" },
+    { value: 4, symbol: "IV" },
+    { value: 1, symbol: "I" }
+  ];
+
+  let result = "";
+  for (let { value, symbol } of romans) {
+    while (num >= value) {
+      result += symbol;
+      num -= value;
     }
-    
-    let roman = "";
-    let str = num.toString();
-    let len = str.length;
-    
-    for (let i = 0; i < len; i++) {
-        let digit = parseInt(str[i]);
-        roman += romanSymbols[len - 1 - i][digit];
-    }
-    
-    return roman;
+  }
+  return result;
 }
+
+// Take input from user
+readline.question('', input => {
+  const num = parseInt(input);
+
+  if (num < 0 || num > 100000) {
+    console.log("Invalid Input. Enter number between 0 and 100000.");
+  } else {
+    console.log(toRoman(num));
+  }
+
+  readline.close();
+});
