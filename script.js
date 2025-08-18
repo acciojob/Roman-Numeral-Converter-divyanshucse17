bf38694c182cb5e9c495beada9ba054c3427941f
divyanshucse17 <divyanshu.cse17@gmail.com>
2025-08-18 08:39:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,28 @@
 function convertToRoman(num) {
-  if (num === 0) return "N"; // Romans used "N" for zero (nulla), optional
-
-  const romanSymbols = [
-    ["M", 1000],
-    ["CM", 900],
-    ["D", 500],
-    ["CD", 400],
-    ["C", 100],
-    ["XC", 90],
-    ["L", 50],
-    ["XL", 40],
-    ["X", 10],
-    ["IX", 9],
-    ["V", 5],
-    ["IV", 4],
-    ["I", 1],
-  ];
-
-  let result = "";
-
-  for (let [symbol, value] of romanSymbols) {
-    while (num >= value) {
-      result += symbol;
-      num -= value;
+    if (num === 0) return ""; // Romans didn't have a numeral for zero
+    
+    const romanSymbols = [
+        ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"], // 1-9
+        ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"], // 10-90
+        ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"], // 100-900
+        ["", "M", "MM", "MMM", "Mↁ", "ↁ", "ↁM", "ↁMM", "ↁMMM", "Mↂ"], // 1000-9000
+        ["", "ↂ", "ↂↂ", "ↂↂↂ", "ↂↇ", "ↇ", "ↇↂ", "ↇↂↂ", "ↇↂↂↂ", "ↂↈ"], // 10000-90000
+        ["", "ↈ"] // 100000
+    ];
+    
+    // Special cases for larger numbers (using Unicode Roman numeral symbols)
+    if (num >= 100000) {
+        return "ↈ".repeat(Math.floor(num / 100000)) + convertToRoman(num % 100000);
     }
-  }
-
-  return result;
+    
+    let roman = "";
+    let str = num.toString();
+    let len = str.length;
+    
+    for (let i = 0; i < len; i++) {
+        let digit = parseInt(str[i]);
+        roman += romanSymbols[len - 1 - i][digit];
+    }
+    
+    return roman;
 }
-
-// --------------------
-// For input/output
-// --------------------
-const readline = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-readline.question("", (input) => {
-  const number = parseInt(input);
-  console.log(convertToRoman(number));
-  readline.close();
-});
