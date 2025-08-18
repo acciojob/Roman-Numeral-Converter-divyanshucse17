4e8e0d472772d687e6afc38b5dddf1453ef7b65a
divyanshucse17 <divyanshu.cse17@gmail.com>
2025-08-18 08:37:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
 function convertToRoman(num) {
-    const romanSymbols = [
-        ['M', 1000],
-        ['CM', 900],
-        ['D', 500],
-        ['CD', 400],
-        ['C', 100],
-        ['XC', 90],
-        ['L', 50],
-        ['XL', 40],
-        ['X', 10],
-        ['IX', 9],
-        ['V', 5],
-        ['IV', 4],
-        ['I', 1]
-    ];
-    
-    let result = '';
-    
-    for (let i = 0; i < romanSymbols.length; i++) {
-        const [symbol, value] = romanSymbols[i];
-        while (num >= value) {
-            result += symbol;
-            num -= value;
-        }
+  if (num === 0) return "N"; // Romans used "N" for zero (nulla), optional
+
+  const romanSymbols = [
+    ["M", 1000],
+    ["CM", 900],
+    ["D", 500],
+    ["CD", 400],
+    ["C", 100],
+    ["XC", 90],
+    ["L", 50],
+    ["XL", 40],
+    ["X", 10],
+    ["IX", 9],
+    ["V", 5],
+    ["IV", 4],
+    ["I", 1],
+  ];
+
+  let result = "";
+
+  for (let [symbol, value] of romanSymbols) {
+    while (num >= value) {
+      result += symbol;
+      num -= value;
     }
-    
-    return result;
+  }
+
+  return result;
 }
 
-// Example usage:
-// console.log(convertToRoman(36)); // Output: XXXVI
-// console.log(convertToRoman(798)); // Output: DCCXCVIII
+// --------------------
+// For input/output
+// --------------------
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question("", (input) => {
+  const number = parseInt(input);
+  console.log(convertToRoman(number));
+  readline.close();
+});
