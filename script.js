cc070ba9eea98d1d6b196e7e58274844edd7e403
divyanshucse17 <divyanshu.cse17@gmail.com>
2025-08-25 15:24:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,30 +4,30 @@ const readline = require('readline').createInterface({
 });
 
 function toRoman(num) {
-  if (num === 0) return "N"; // Romans sometimes used N for zero
+  if (num === 0) return "N"; // Romans used "N" (nulla) for zero
 
   const romans = [
-    { value: 100000, symbol: "C̅" },
-    { value: 90000, symbol: "X̅C̅" },
-    { value: 50000, symbol: "L̅" },
-    { value: 40000, symbol: "X̅L̅" },
-    { value: 10000, symbol: "X̅" },
-    { value: 9000, symbol: "I̅X̅" },
-    { value: 5000, symbol: "V̅" },
-    { value: 4000, symbol: "I̅V̅" },
-    { value: 1000, symbol: "M" },
-    { value: 900, symbol: "CM" },
-    { value: 500, symbol: "D" },
-    { value: 400, symbol: "CD" },
-    { value: 100, symbol: "C" },
-    { value: 90, symbol: "XC" },
-    { value: 50, symbol: "L" },
-    { value: 40, symbol: "XL" },
-    { value: 10, symbol: "X" },
-    { value: 9, symbol: "IX" },
-    { value: 5, symbol: "V" },
-    { value: 4, symbol: "IV" },
-    { value: 1, symbol: "I" }
+    { value: 100000, symbol: "C̅" },   // 100,000
+    { value: 90000, symbol: "X̅C̅" },  // 90,000
+    { value: 50000, symbol: "L̅" },    // 50,000
+    { value: 40000, symbol: "X̅L̅" },  // 40,000
+    { value: 10000, symbol: "X̅" },    // 10,000
+    { value: 9000, symbol: "I̅X̅" },   // 9,000
+    { value: 5000, symbol: "V̅" },     // 5,000
+    { value: 4000, symbol: "I̅V̅" },   // 4,000
+    { value: 1000, symbol: "M" },      // 1,000
+    { value: 900, symbol: "CM" },      // 900
+    { value: 500, symbol: "D" },       // 500
+    { value: 400, symbol: "CD" },      // 400
+    { value: 100, symbol: "C" },       // 100
+    { value: 90, symbol: "XC" },       // 90
+    { value: 50, symbol: "L" },        // 50
+    { value: 40, symbol: "XL" },       // 40
+    { value: 10, symbol: "X" },        // 10
+    { value: 9, symbol: "IX" },        // 9
+    { value: 5, symbol: "V" },         // 5
+    { value: 4, symbol: "IV" },        // 4
+    { value: 1, symbol: "I" }          // 1
   ];
 
   let result = "";
@@ -44,7 +44,7 @@ function toRoman(num) {
 readline.question('', input => {
   const num = parseInt(input);
 
-  if (num < 0 || num > 100000) {
+  if (isNaN(num) || num < 0 || num > 100000) {
     console.log("Invalid Input. Enter number between 0 and 100000.");
   } else {
     console.log(toRoman(num));
